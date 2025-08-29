2dc959cdc0f018222c43247884b30fa242205408
mishuMUSICOPYT <mishumusicyt@gmail.com>
2025-08-29 20:26:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,15 +41,15 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MAHADEV-YT/Rajababy",
+    "https://github.com/mishuMUSICOPYT/RahaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/StylishNameFont")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+aUamAA43_p4xMWE1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/love_shayari_hub")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+lhZd-SXS-AY2NGZl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
@@ -126,6 +126,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
