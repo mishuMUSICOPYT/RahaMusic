cd2d766a0553918aa8da2194fdd967712d00edd9
mishuMUSICOPYT <mishumusicyt@gmail.com>
2025-09-01 10:54:56 +0530

    Update Aviaxmusic.py

--- AviaxMusic/plugins/tools/Aviaxmusic.py
+++ AviaxMusic/plugins/tools/Aviaxmusic.py
@@ -3,9 +3,9 @@ import random
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import FloodWait
-from Aviaxmusic import app
-from Aviaxmusic.utils.admin_check import is_admin
-from Aviaxmusic.plugins.misc.funtag_messages import (
+from AviaxMusic import app
+from AviaxMusic.utils.admin_check import is_admin
+from AviaxMusic.plugins.misc.funtag_messages import (
     GN_MESSAGES,
     GM_MESSAGES,
     HI_MESSAGES,
